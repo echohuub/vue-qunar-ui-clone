699ce756be770a95fe8e48508e9aa0cf3d39720d
heqingbao <qingbao.ho@gmail.com>
2019-04-20 23:22:52 +0800

    详情页--Banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img"
+           src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg"/>
+      <div class="banner-info">
+        <div class="banner-title">
+          故宫(AAAAA景区)
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe626;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs"
+                    v-show="showGallary"
+                    @close="handleGalleryClose">
+    </common-gallary>
+  </div>
+</template>
+
+<script>
+  import CommonGallary from 'common/gallary/Gallary'
+
+  export default {
+    name: 'DetailBanner',
+    data () {
+      return {
+        showGallary: false,
+        imgs: ['http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_r_800x800_6edd8174.jpg',
+          'http://img1.qunarzz.com/sight/p55/201211/04/fbcab3e5d6479ce893835fbb.jpg_r_800x800_6360f514.jpg']
+      }
+    },
+    components: {
+      CommonGallary
+    },
+    methods: {
+      handleBannerClick () {
+        this.showGallary = true
+      },
+      handleGalleryClose () {
+        this.showGallary = false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+
+    .banner-img
+      width 100%
+
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+
+      .banner-number
+        height .32rem
+        margin-top .14rem
+        padding 0 .4rem
+        line-height .32rem
+        border-radius .14rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+
+        .banner-icon
+          font-size .24rem
+</style>
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,7 +2,10 @@
   <div>
     <div class="recommend-title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.id">
+      <router-link tag="li"
+                   class="item border-bottom"
+                   v-for="item of list" :key="item.id"
+                   :to="'/detail/' + item.id">
         <img class="item-img" :src="item.imgUrl"/>
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
@@ -10,7 +13,7 @@
           <button class="item-button">查看详情</button>
         </div>
 
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
